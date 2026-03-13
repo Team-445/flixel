a2b6650716a84e8be491065d2b5a691e854c77f2
Ralty <78720179+Raltyro@users.noreply.github.com>
2026-03-13 10:15:09 +0700

    Fix `focusLostFrame`

--- flixel/FlxGame.hx
+++ flixel/FlxGame.hx
@@ -354,6 +354,8 @@ class FlxGame extends Sprite
 		FlxG.signals.focusGained.dispatch();
 		_state.onFocus();
 
+		stage.frameRate = FlxG.drawFramerate;
+
 		if (!FlxG.autoPause)
 			return;
 
@@ -365,8 +367,6 @@ class FlxGame extends Sprite
 		#if FLX_DEBUG
 		debugger.stats.onFocus();
 		#end
-
-		stage.frameRate = FlxG.drawFramerate;
 		#if FLX_SOUND_SYSTEM
 		FlxG.sound.onFocus();
 		#end
@@ -384,6 +384,8 @@ class FlxGame extends Sprite
 		FlxG.signals.focusLost.dispatch();
 		_state.onFocusLost();
 
+		stage.frameRate = focusLostFramerate;
+
 		if (!FlxG.autoPause)
 			return;
 
@@ -395,8 +397,6 @@ class FlxGame extends Sprite
 		#if FLX_DEBUG
 		debugger.stats.onFocusLost();
 		#end
-
-		stage.frameRate = focusLostFramerate;
 		#if FLX_SOUND_SYSTEM
 		FlxG.sound.onFocusLost();
 		#end
