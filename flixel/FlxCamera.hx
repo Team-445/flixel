262f9f39b0d6f741d02e9a4ae0cd9c091ade396b
MAJigsaw77 <mihaialexandru427@yahoo.com>
2026-05-07 13:25:39 +0300

    Revert "fix rounding for non-opaque cameras"
    This reverts commit 89280ac92fdfe5bb35e8984417598831b6287e03.

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -166,7 +166,7 @@ class FlxCamera extends FlxBasic
 	 * The natural background color of the camera, in `AARRGGBB` format. Defaults to `FlxG.cameras.bgColor`.
 	 * On Flash, transparent backgrounds can be used in conjunction with `useBgAlphaBlending`.
 	 */
-	public var bgColor(default, set):FlxColor;
+	public var bgColor:FlxColor;
 
 	/**
 	 * Sometimes it's easier to just work with a `FlxSprite`, than it is to work directly with the `BitmapData` buffer.
@@ -1371,14 +1371,13 @@ class FlxCamera extends FlxBasic
 	function updateScrollRect():Void
 	{
 		var rect:Rectangle = (_scrollRect != null) ? _scrollRect.scrollRect : null;
-		var isTransparent:Bool = (bgColor == FlxColor.TRANSPARENT);
 
 		if (rect != null)
 		{
 			rect.x = rect.y = 0;
 
-			rect.width = Math.round(width * initialZoom * FlxG.scaleMode.scale.x) + (isTransparent ? 0 : -2);
-			rect.height = Math.round(height * initialZoom * FlxG.scaleMode.scale.y) + (isTransparent ? 0 : -2);
+			rect.width = width * initialZoom * FlxG.scaleMode.scale.x;
+			rect.height = height * initialZoom * FlxG.scaleMode.scale.y;
 
 			_scrollRect.scrollRect = rect;
 
@@ -1985,16 +1984,6 @@ class FlxCamera extends FlxBasic
 		return Color;
 	}
 
-	function set_bgColor(Color:FlxColor):FlxColor
-	{
-		if (bgColor != Color)
-		{
-			bgColor = Color;
-			updateScrollRect();
-		}
-
-		return Color;
-	}
 	function set_antialiasing(Antialiasing:Bool):Bool
 	{
 		antialiasing = Antialiasing;
